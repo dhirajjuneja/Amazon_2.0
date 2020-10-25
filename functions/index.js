1c081906a525ad3f12bc7027d6a1f7ac27a65aa6
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-10-25 22:19:19 +0530

    reactjs

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HejzyCdekoq3yLZrmP8RkNxNVplL7Jfmb5q51fLGBWYLX9PYSD8HIVCzu9WbEyjsINLEhdAbgA9VMWL5EWhdDYL00f240bFbe"
+);
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/dj-amaz-portfoliyo/us-central1/api
--- src/Payment.js
+++ src/Payment.js
@@ -1,22 +1,60 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import React, { useState } from "react";
-import CurrencyFormat from "react-currency-format";
-import { Link } from "react-router-dom";
-import CheckoutProduct from "./CheckoutProduct";
-import "./Payment.css";
-import { getBasketTotal } from "./reducer";
+import React, { useState, useEffect } from 'react';
+import './Payment.css';
 import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link, useHistory } from "react-router-dom";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import axios from './axios';
 
 function Payment() {
-  const [{ user, basket }] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+  const history = useHistory();
 
   const stripe = useStripe();
   const elements = useElements();
 
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
   const [error, setError] = useState(null);
   const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
 
-  const handleSubmit = (e) => {};
+  useEffect(() => {
+    // generate the special stripe secret which allows us to charge a customer
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        // Stripe expects the total in a currencies subunits
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+  console.log("THE SECRET IS >>>", clientSecret);
+  console.log("👱", user);
+
+  const handleSubmit = async (event) => {
+    // do all the fancy stripe stuff...
+    event.preventDefault();
+    setProcessing(true);
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+        history.replace("/orders");
+      });
+  };
   const handleChange = (event) => {
     // Listen for changes in the CardElement
     // and display any errors as the customer types their card details
@@ -76,6 +114,9 @@ function Payment() {
                   prefix={"$"}
                 />
               </div>
+              <button disabled={processing || disabled || succeeded}>
+                <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+              </button>
             </form>
           </div>
         </div>
