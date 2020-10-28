bbc64cf0c9982e5eb563ab537362dd816533ac20
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-10-28 13:59:34 +0530

    Amazon_Clone

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,41 +1,36 @@
 import React from "react";
 import "./Subtotal.css";
-import CurrecyFormat from "react-currency-format";
+import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
 import { useHistory } from "react-router-dom";
 
 function Subtotal() {
   const history = useHistory();
-  const [{ basket }] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
-      <CurrecyFormat
+      <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket.length} items):
-              <strong>{value}</strong>
+              {/* Part of the homework */}
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
-              <input type="checkbox" />
-              The order contains a gift
+              <input type="checkbox" /> This order contains a gift
             </small>
           </>
         )}
         decimalScale={2}
-        value={getBasketTotal(basket)}
+        value={getBasketTotal(basket)} // Part of the homework
         displayType={"text"}
-        thousandSeperator={true}
-        prefix={"₹"}
+        thousandSeparator={true}
+        prefix={"$"}
       />
 
-      <button
-        className="subtotal__payment"
-        onClick={(e) => history.push("/payment")}
-      >
-        Proceed to Checkout
-      </button>
+      <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
     </div>
   );
 }
