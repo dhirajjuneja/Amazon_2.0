89424127bf021acb93c7e13280be4fe52fc6b069
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-10-28 13:59:22 +0530

    Amazon_Clone

--- src/reducer.js
+++ src/reducer.js
@@ -1,51 +1,57 @@
 export const initialState = {
   basket: [],
+  
   user: null,
 };
 
-//Selector
-export function getBasketTotal(basket) {
-  return basket?.reduce((amount, item) => item.price + amount, 0);
-}
-export function getBasketItems(basket) {
-  return basket?.reduce((name, item) => (item.name = name), 0);
-}
+// Selector
+export const getBasketTotal = (basket) => 
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
+  console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
+    
+    case 'EMPTY_BASKET':
+      return {
+        ...state,
+        basket: []
+      }
 
     case "REMOVE_FROM_BASKET":
       const index = state.basket.findIndex(
         (basketItem) => basketItem.id === action.id
       );
       let newBasket = [...state.basket];
+
       if (index >= 0) {
         newBasket.splice(index, 1);
+
       } else {
         console.warn(
-          `Can't remove product (id: ${action.id}) as it is not in the basket!`
-        );
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        )
       }
+
       return {
         ...state,
-        basket: newBasket,
-      };
-    case "EMPTY_BASKET":
-      return {
-        ...state,
-        basket: [],
-      };
+        basket: newBasket
+      }
+    
     case "SET_USER":
       return {
         ...state,
-        user: action.user,
-      };
+        user: action.user
+      }
+
     default:
       return state;
   }
 };
+
 export default reducer;
